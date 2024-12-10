949e10fb25c38256f821c3788cd2d3d0650408fb
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2024-12-10 17:17:22 +0100

    Add files via upload

---
+++ 16_firstWhile/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let a = [0, 1, 2, 3, 4];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("The value is: {}", a[index]);
+        index += 1;
+    }
+}
---
+++ 17_secondFor/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let a = [0, 1, 2, 3, 4];
+
+    for element in a {
+        println!("The value is: {element}");
+    }
+}
